090143c04b9b83ea6969623225ec155b84681b08
Andreas Havenstein <ah@it-agile.de>
2019-08-19 09:08:44 +0200

    Choose randomly the direction if multiple chase directions are possible.

--- spec/GhostChasingMoveDeciderSpec.js
+++ spec/GhostChasingMoveDeciderSpec.js
@@ -53,21 +53,59 @@ describe("ChasingMoveDecider", function () {
             options: [Constants.DIRECTION_DOWN, Constants.DIRECTION_UP],
             expectedMove: Constants.DIRECTION_UP
         },
+        // more than one possible move:
+        {
+            ghostX: 1,
+            ghostY: 1,
+            playerX: 0,
+            playerY: 0,
+            options: [Constants.DIRECTION_RIGHT, Constants.DIRECTION_DOWN, Constants.DIRECTION_LEFT, Constants.DIRECTION_UP],
+            expectedMovesForFallbackMoveDecider: [Constants.DIRECTION_LEFT, Constants.DIRECTION_UP]
+        },
+        {
+            ghostX: 0,
+            ghostY: 0,
+            playerX: 1,
+            playerY: 1,
+            options: [Constants.DIRECTION_RIGHT, Constants.DIRECTION_DOWN, Constants.DIRECTION_LEFT, Constants.DIRECTION_UP],
+            expectedMovesForFallbackMoveDecider: [Constants.DIRECTION_RIGHT, Constants.DIRECTION_DOWN]
+        },
+        {
+            ghostX: 1,
+            ghostY: 0,
+            playerX: 0,
+            playerY: 1,
+            options: [Constants.DIRECTION_RIGHT, Constants.DIRECTION_DOWN, Constants.DIRECTION_LEFT, Constants.DIRECTION_UP],
+            expectedMovesForFallbackMoveDecider: [Constants.DIRECTION_LEFT, Constants.DIRECTION_DOWN]        
+        },
+        {
+            ghostX: 0,
+            ghostY: 1,
+            playerX: 1,
+            playerY: 0,
+            options: [Constants.DIRECTION_RIGHT, Constants.DIRECTION_DOWN, Constants.DIRECTION_LEFT, Constants.DIRECTION_UP],
+            expectedMovesForFallbackMoveDecider: [Constants.DIRECTION_UP, Constants.DIRECTION_RIGHT]
+        },
+
     ];
     parameters.forEach((parameter) => {
         it("should find move which heads to the player position", function () {
-            let chasingMoveDecider = new ChasingMoveDecider();
+            let fallbackMoveDecider = jasmine.createSpyObj("fallbackMoveDecider", ["chooseMove"]);
+            let chasingMoveDecider = new ChasingMoveDecider(fallbackMoveDecider);
             let ghost = jasmine.createSpyObj("ghost", {getPosX: parameter.ghostX, getPosY: parameter.ghostY});
             let player = jasmine.createSpyObj("player", {getPosX: parameter.playerX, getPosY: parameter.playerY});
-            let options = [Constants.DIRECTION_RIGHT, Constants.DIRECTION_DOWN, Constants.DIRECTION_LEFT, Constants.DIRECTION_UP];
             let move = chasingMoveDecider.chooseMove(parameter.options, ghost, player);
-
-            expect(move).toBe(parameter.expectedMove);
+            if (parameter.expectedMove) {
+                expect(move).toBe(parameter.expectedMove);
+            } else {
+                expectAllPossibleDirectionsArePassedToFallback(fallbackMoveDecider, parameter);
+            }
+            
         })
     });
 
     it("should use fallback move decider if no chasing direction can be used", ()=>{
-        let fallbackMoveDecider = jasmine.createSpyObj("randomMoveDecider", {chooseMove: Constants.DIRECTION_RIGHT});
+        let fallbackMoveDecider = jasmine.createSpyObj("fallbackMoveDecider", {chooseMove: Constants.DIRECTION_RIGHT});
         let chasingMoveDecider = new ChasingMoveDecider(fallbackMoveDecider);
         let ghost = jasmine.createSpyObj("ghost", {getPosX: 1, getPosY: 1});
         let player = jasmine.createSpyObj("player", {getPosX: 0, getPosY: 0});
@@ -79,3 +117,12 @@ describe("ChasingMoveDecider", function () {
     });
 
 });
+
+function expectAllPossibleDirectionsArePassedToFallback(fallbackMoveDecider, parameter) {
+    expect(fallbackMoveDecider.chooseMove.calls.count()).toBe(1);
+    let chooseMoveArgs = fallbackMoveDecider.chooseMove.calls.argsFor(0)[0];
+    expect(chooseMoveArgs.length).toBe(parameter.expectedMovesForFallbackMoveDecider.length);
+    chooseMoveArgs.forEach(direction => {
+        expect(parameter.expectedMovesForFallbackMoveDecider.includes(direction)).toBeTruthy();
+    });
+}
--- src/model/chasingMoveDecider.js
+++ src/model/chasingMoveDecider.js
@@ -7,26 +7,35 @@ export default class ChasingMoveDecider {
     }
 
     chooseMove(options, ghost, player) {
+        let possibleOptions = [];
         if (ghost.getPosX()> player.getPosX()) {
             if (options.includes(Constants.DIRECTION_LEFT)) {
-                return Constants.DIRECTION_LEFT;
+                possibleOptions.push(Constants.DIRECTION_LEFT);
             }
         } 
         if (ghost.getPosX()< player.getPosX()) {
             if (options.includes(Constants.DIRECTION_RIGHT)) {
-                return Constants.DIRECTION_RIGHT;
+                possibleOptions.push(Constants.DIRECTION_RIGHT);
             }
         }
         if (ghost.getPosY()> player.getPosY()) {
             if (options.includes(Constants.DIRECTION_UP)) {
-                return Constants.DIRECTION_UP;
+                possibleOptions.push(Constants.DIRECTION_UP);
             }
         }
         if (ghost.getPosY()< player.getPosY()) {
             if (options.includes(Constants.DIRECTION_DOWN)) {
-                return Constants.DIRECTION_DOWN;
+                possibleOptions.push(Constants.DIRECTION_DOWN);
             }
         }
-        return this.fallbackMoveDecider.chooseMove(options, ghost, player);
+        if (possibleOptions.length === 1){
+            return possibleOptions[0];
+        } 
+        if (possibleOptions.length===0) {
+            possibleOptions = options;
+        }
+        // The current direction will not be regarded as a preferred direction for the fallback decider.
+        // Should a chaser prefer the current direction if no chasing direction is possible?
+        return this.fallbackMoveDecider.chooseMove(possibleOptions, ghost, player);
     }
 }
